aa63a729359a2da19f62c7610b74c7962d5abbfa
HowWof <howwof@protonmail.com>
2024-02-13 12:51:58 +0100

    nashc: Drop RenderScript
    Change-Id: Ifca6b5b6d9e8de8c752643ea470f76a21100ea84

--- device.mk
+++ device.mk
@@ -336,10 +336,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio@1.6.vendor
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6785 \
